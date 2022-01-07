f9cb794937b9018f0a8e4644ce811841bcac8958
Jose Maria Paez <jmpsok@gmail.com>
2022-01-07 23:08:12 +0100

    cambio en service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,9 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if (env('REDIRECT_HTTPS')) {
+        /* if (env('REDIRECT_HTTPS')) {
             $this->app['request']->server->set('HTTPS', true);
-        }
+        } */
     }
 
     /**
@@ -26,8 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        if (env('REDIRECT_HTTPS')) {
+        /* if (env('REDIRECT_HTTPS')) {
             $url->formatScheme('https://');
-        }
+        } */
     }
 }
